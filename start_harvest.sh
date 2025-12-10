0a5aedb0f4b9ff4a751736ce1526aa1fe3eb045c
Tamer Vassib <tvassib@gmail.com>
2025-12-10 23:35:43 +0000

    .sh update

--- start_harvest.sh
+++ start_harvest.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# 1. Define a log file for the Python script ITSELF (so we can see if Python crashes)
+
+# 2. Define Log File (Hardcoded full path is safer)
 LOGFILE="/home/tnv/Desktop/ClashOfClansDataHarvester/harvest_debug.log"
-sleep 60
-# 2. Go to the directory
+
+# 3. Move to directory
 cd /home/tnv/Desktop/ClashOfClansDataHarvester/ || exit
 
-# 3. CRITICAL FIX: Tell screen what kind of terminal to use
+# 4. Set Terminal
 export TERM=xterm
 
-# 4. Run screen
-# We modify the command to redirect Python's output to our logfile INSIDE the screen session.
-# If Python crashes, the error will be in 'harvest_debug.log'
-/usr/bin/screen -dmS Harvest bash -c "./venv/bin/python3 tracker.py > $LOGFILE 2>&1"
+# 5. Initialization Timestamp (Writes directly to file, bypassing Python)
+echo "--- NEW REBOOT SESSION STARTED AT $(date) ---" >> "$LOGFILE"
 
+# 6. Run Screen
+# The -u flag forces Python to print immediately.
+/usr/bin/screen -dmS Harvest bash -c "./venv/bin/python3 -u tracker.py >> $LOGFILE 2>&1"
